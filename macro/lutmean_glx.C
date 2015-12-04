3e161a5ec83e49626094af8d5eb950becdd84b69
Roman Dzhygadlo <r.dzhygadlo@gsi.de>
2015-12-04 15:39:52 +0100

    48 luts

--- macro/lutmean_glx.C
+++ macro/lutmean_glx.C
@@ -1,4 +1,4 @@
-void lutmean_glx(TString baseFile = "../data/lut.root", Int_t b=0){
+void lutmean_glx(TString baseFile = "../data/lut.root"){
   gROOT->ProcessLine(".L ../src/GlxLutNode.cxx+");
   gInterpreter->GenerateDictionary("vector<TVector3>","TVector3.h"); 
   
@@ -13,17 +13,20 @@ void lutmean_glx(TString baseFile = "../data/lut.root", Int_t b=0){
 
  
   TFile *fFileNew = TFile::Open(outFile, "RECREATE");
-  TClonesArray *fLutNew;
+  TClonesArray *fLutNew[48];
 
   TTree *fTreeNew = new TTree("prtlut","Look-up table for DIRC. Averaged");
 
-  fLutNew = new TClonesArray("GlxLutNode");
   fTreeNew->Branch("LUT",&fLutNew,256000,2); 
 
   Int_t Nnodes = 20000;
-  TClonesArray &fLutaNew = *fLutNew;
-  for (Long64_t n=0; n<Nnodes; n++) {
-    new((fLutaNew)[n]) GlxLutNode(-1);
+  for(Int_t l=0; l<48; l++){
+    fLutNew[l] = new TClonesArray("GlxLutNode");
+    fTreeNew->Branch(Form("LUT_%d",b),&fLutNew[l],256000,2);
+    TClonesArray &fLutaNew = *fLutNew[l];
+    for (Long64_t n=0; n<Nnodes; n++) {
+      new((fLutaNew)[n]) GlxLutNode(-1);
+    }
   }
 
   // TCanvas* c = new TCanvas("c","c",0,0,800,1200); c->Divide(1,2);
@@ -40,64 +43,66 @@ void lutmean_glx(TString baseFile = "../data/lut.root", Int_t b=0){
   Double_t angle, minangle,pathid,time,sumt;
   GlxLutNode *node;
 
-  for (Int_t inode=0; inode<fLut->GetEntriesFast(); inode++){
-    if(inode%1000==0) cout<<"Node # "<<inode<<endl;
-    node= (GlxLutNode*) fLut->At(inode);
-    //histNode->Fill(node->GetNodeId(),node->Entries());
-    Int_t size = node->Entries();    
-    if(size<1) continue;
-    for(int i=0; i<size; i++){
-      dir = node->GetEntry(i);
-      time = node->GetTime(i);
-      pathid = node->GetPathId(i);
+  for(Int_t l=0; l<48; l++){
+    for (Int_t inode=0; inode<fLut->GetEntriesFast(); inode++){
+      if(inode%1000==0) cout<<"Node # "<<inode<<endl;
+      node= (GlxLutNode*) fLut->At(inode);
+      //histNode->Fill(node->GetNodeId(),node->Entries());
+      Int_t size = node->Entries();    
+      if(size<1) continue;
+      for(int i=0; i<size; i++){
+	dir = node->GetEntry(i);
+	time = node->GetTime(i);
+	pathid = node->GetPathId(i);
       
-      // hDir->Fill(dir.X());
-      // hTime->Fill(time);
+	// hDir->Fill(dir.X());
+	// hTime->Fill(time);
 
-      bool newid = true;
-      for(int j=0; j<pArray.size(); j++){
-	if(pathid == pArray[j]){
-	  vArray[j].push_back(dir);
-	  tArray[j].push_back(time);
-	  newid= false;
+	bool newid = true;
+	for(int j=0; j<pArray.size(); j++){
+	  if(pathid == pArray[j]){
+	    vArray[j].push_back(dir);
+	    tArray[j].push_back(time);
+	    newid= false;
+	  }
+	}
+	if(newid) {
+	  vArray[pArray.size()].push_back(dir);
+	  tArray[pArray.size()].push_back(time);
+	  pArray.push_back(pathid);
 	}
       }
-      if(newid) {
-	vArray[pArray.size()].push_back(dir);
-	tArray[pArray.size()].push_back(time);
-	pArray.push_back(pathid);
-      }
-    }
   
-    for(int j=0; j<pArray.size(); j++){
-      sum = TVector3(0,0,0);
-      sumt=0;
-      for(int v=0; v<vArray[j].size(); v++) {
-	sum += vArray[j][v]; 
-	sumt += tArray[j][v]; 
+      for(int j=0; j<pArray.size(); j++){
+	sum = TVector3(0,0,0);
+	sumt=0;
+	for(int v=0; v<vArray[j].size(); v++) {
+	  sum += vArray[j][v]; 
+	  sumt += tArray[j][v]; 
 
-	// hDir->Fill(vArray[j][v].X());
-	// hTime->Fill(tArray[j][v]);
+	  // hDir->Fill(vArray[j][v].X());
+	  // hTime->Fill(tArray[j][v]);
+	}
+      
+	// c->cd(1);
+	// hTime->Draw();
+	// c->cd(2);
+	// hDir->Draw();
+	// c->Update();  
+	// c->WaitPrimitive();
+	// hDir->Reset();
+	// hTime->Reset();
+
+	if(vArray[j].size()<1) continue;
+	Double_t weight = 1/(Double_t)vArray[j].size(); 
+	sum *= weight;
+	sumt *= weight;
+      
+	((GlxLutNode*)(fLutNew[l]->At(inode)))->AddEntry(node->GetDetectorId(), sum,j,node->GetNRefl(0),sumt, node->GetDigiPos(),node->GetDigiPos(),vArray[j].size()/(Double_t)size); 
       }
-      
-      // c->cd(1);
-      // hTime->Draw();
-      // c->cd(2);
-      // hDir->Draw();
-      // c->Update();  
-      // c->WaitPrimitive();
-      // hDir->Reset();
-      // hTime->Reset();
-
-      if(vArray[j].size()<1) continue;
-      Double_t weight = 1/(Double_t)vArray[j].size(); 
-      sum *= weight;
-      sumt *= weight;
-      
-      ((GlxLutNode*)(fLutNew->At(inode)))->AddEntry(node->GetDetectorId(), sum,j,node->GetNRefl(0),sumt, node->GetDigiPos(),node->GetDigiPos(),vArray[j].size()/(Double_t)size); 
+      for(int i=0; i<100; i++) {vArray[i].clear();  tArray[i].clear();}
+      pArray.clear();
     }
-    for(int i=0; i<100; i++) {vArray[i].clear();  tArray[i].clear();}
-    pArray.clear();
   }
 
   fTreeNew->Fill();
